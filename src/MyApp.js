da8f52cd600a4605e583fb56f17c625be620f1ab
minadedijer <mina-d@live.com>
2021-04-11 20:11:52 -0700

    first mybranch

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,9 +1,33 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Table from './Table';
 import Form from './Form';
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);  
+
+async function fetchAll(){
+   try {
+      const response = await axios.get('http://localhost:5000/users');
+      return response.data.users_list;     
+   }
+   catch (error){
+      //We're not handling errors. Just logging into the console.
+      console.log(error); 
+      return false;         
+   }
+}
+
+async function makePostCall(person){
+   try {
+      const response = await axios.post('http://localhost:5000/users', person);
+      return response;
+   }
+   catch (error) {
+      console.log(error);
+      return false;
+   }
+}
 
 function removeOneCharacter (index) {
   const updated = characters.filter((character, i) => {
@@ -11,9 +35,19 @@ function removeOneCharacter (index) {
     });
     setCharacters(updated);
 }
-function updateList(person) {
-  setCharacters([...characters, person]);
+function updateList(person) { 
+   makePostCall(person).then( result => {
+   if (result)
+      setCharacters([...characters, person] );
+   });
 }
+useEffect(() => {
+   fetchAll().then( result => {
+      if (result)
+         setCharacters(result);
+    });
+}, [] );
+
 return (
   <div className="container">
     <Table characterData={characters} removeCharacter={removeOneCharacter} />
